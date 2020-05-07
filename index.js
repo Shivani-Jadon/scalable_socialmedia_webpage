591055dc69414f228d6b97965c4df82b82fde880
Shivani <shivanijadon98@gmail.com>
2020-05-07 15:36:32 +0530

    add stylesheet of all pages

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ const app = express();
 const port = 8000;
 const express_layout = require('express-ejs-layouts');
 
+// directing app to assets folder for static files
+app.use(express.static('./assets'));
+
 //loading layouts for ejs views
 app.use(express_layout);
+//extracting styles and scripts from subfiles into the layout
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
 
 //loading router
 app.use('/', require("./routes"));
