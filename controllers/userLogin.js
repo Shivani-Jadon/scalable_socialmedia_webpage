7e751eb716a6d072f76be6d6170b5bff7f7115ab
Shivani <shivanijadon98@gmail.com>
2020-05-19 12:40:09 +0530

    add sign out to profile page

--- controllers/userLogin.js
+++ controllers/userLogin.js
@@ -1,5 +1,8 @@
 // rendering user login view
 module.exports.validate_user = function(req, res){
 
+    // clearing stored cookies before new login
+    res.clearCookie('user_id');
+
     return res.render("login", {title : "User Login page"});
 }
