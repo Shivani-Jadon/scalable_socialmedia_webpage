d2cd461823c994568c8d9f3bf5bfbf49f40d408d
Shivani <shivanijadon98@gmail.com>
2020-05-19 10:53:32 +0530

    fix minor changes

--- controllers/userSignup.js
+++ controllers/userSignup.js
@@ -1,4 +1,4 @@
-// rendering user signin view
+// rendering user signup view
 module.exports.create_user = function(req, res){
 
     return res.render("signup", {title : "User Sign-up page"});
--- routes/signup.js
+++ routes/signup.js
@@ -2,7 +2,7 @@ const express = require("express");
 const route = express.Router();
 
 // storing information of user-login controller
-const signup_user = require("../controllers/userSignup");
+const signup_user = require("../controllers/usersignup");
 
 console.log("loading user signup route");
 
