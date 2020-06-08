c29e045a9652eeed0c0f1c4e16cb7d1c807c8b19
Shivani <shivanijadon98@gmail.com>
2020-06-09 02:58:03 +0530

    display post in json and delete post without authentication using API

--- controllers/api/v1/postsAPI.js
+++ controllers/api/v1/postsAPI.js
@@ -1,7 +1,55 @@
-module.exports.post_data = function(req,res){
+const Post = require("../../../models/post_model");
+const Comment = require("../../../models/comment_model");
+
+module.exports.post_data = async function(req,res){
+
+    let posts = await Post.find({})
+                        .sort('-createdAt')
+                        .populate({ path : 'userInfo', select: ['first_name', 'last_name', 'email', 'createdAt', 'updatedAt']})
+                        .populate({path : 'comments', options: { sort: '-createdAt' },       
+                                populate : {
+                                    path : 'user',
+                                    select: ['first_name', 'last_name', 'email', 'createdAt', 'updatedAt']
+                                }});
 
     return res.json(200, {
         message : "API for post data in json",
-        post : []
+        posts : posts
     })
+    
+}
+
+
+// controller for deleting post without authentication (without jwt)
+module.exports.destroy = async function(req, res){
+
+    try{
+
+        let post = await Post.findById(req.params.id);
+
+        // .id will convert id object to string for comparision
+        // if(post.userInfo == req.user.id)
+        // {
+            post.remove();
+
+            await Comment.deleteMany({post: req.params.id});
+            
+        
+            return res.json(200, {
+                message : "Post deleted successfully"
+            });
+    
+        // }else{
+            
+        //     req.flash("error", "You are not authorised to remove Post");
+        //     return res.redirect("back");
+        // }
+
+    }catch(err){
+        console.log("******", err);
+        return res.json(200, {
+            message : "Internal Server error"
+        });
+    }
+        
 }
--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -4,4 +4,7 @@ const route = express.Router();
 const postApi = require("../../../controllers/api/v1/postsAPI");
 route.get("/post_api", postApi.post_data);
 
+// route for post deletion
+route.get("/delete_post/:id", postApi.destroy);
+
 module.exports = route;
