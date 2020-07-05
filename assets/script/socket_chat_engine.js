b1aefb5e1665c1403551b4628e2872e0017c62d8
Shivani <shivanijadon98@gmail.com>
2020-07-06 01:16:09 +0530

    create chatroom via sockets

--- assets/script/socket_chat_engine.js
+++ assets/script/socket_chat_engine.js
@@ -1,22 +1,38 @@
-// this socket is for front-end
+// this socket is for front-end/Subscribers
+// Client-Server
+
 class ChatEngine{
 
-    constructor(chatBoxId, userId){
+    constructor(chatBoxId, userEmail){
         this.chatBox = $(`#${chatBoxId}`);
-        this.userId = userId;
+        this.userEmail = userEmail;
 
         this.socket = io.connect("http://localhost:4000");
 
         // check user before extablishing connection
-        if(this.userId){
+        if(this.userEmail){
             this.connectionHandler();
         }
     }
 
     connectionHandler(){
+        let self = this;
+
         // on request to connect sockets
         this.socket.on('connect', function(){
             console.log("Connection has been established using Socket....");
+        })
+
+        // emit request to join
+        // the client will join the room already created or a new room is created
+        self.socket.emit('join_room', {
+            user_email : self.userEmail,
+            chatroom : 'Scafel'
+        });
+
+        // ON receiving acknowledgement the client is notified
+        self.socket.on('user_joined', function(data){
+            console.log("A new User joined the chat room ",data);
         })
     }
 }
--- config/chatEngine_socket.js
+++ config/chatEngine_socket.js
@@ -1,4 +1,5 @@
-// Socket for server
+// Socket for server/Observer
+// Server to client
 
 module.exports.chatSocketIO = function(serverSocket){
     let io = require('socket.io')(serverSocket);
@@ -7,9 +8,22 @@ module.exports.chatSocketIO = function(serverSocket){
     io.sockets.on('connection', function(socket){
         console.log("New Connection received from ",socket.id);
 
+        // ON is fired when an event is received
+        // EMIT is fired when an event is sent/acknowledged
         // when this connection is disconnected/lost due to any reason such as page refresh
         socket.on('disconnect', function(){
             console.log("Connection Lost with Socket");
         }) 
+
+        // ON receiving room joining request from client
+        socket.on('join_room', function(data){
+            console.log("Joining request received ", data);
+
+            // creating or joining chatroom
+            socket.join(data.chatroom);
+
+            // EMIT acknowledgement from server to user that user has joined the chatroom
+            io.in(data.chatroom).emit('user_joined', data);
+        });
     });
 }
