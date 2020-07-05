10f8fbb1a1bec962be77e3d7143298c3ca58a260
Shivani <shivanijadon98@gmail.com>
2020-07-06 00:37:04 +0530

    extablish sockets connection

--- assets/script/socket_chat_engine.js
+++ assets/script/socket_chat_engine.js
@@ -1,1 +1,22 @@
 // this socket is for front-end
+class ChatEngine{
+
+    constructor(chatBoxId, userId){
+        this.chatBox = $(`#${chatBoxId}`);
+        this.userId = userId;
+
+        this.socket = io.connect("http://localhost:4000");
+
+        // check user before extablishing connection
+        if(this.userId){
+            this.connectionHandler();
+        }
+    }
+
+    connectionHandler(){
+        // on request to connect sockets
+        this.socket.on('connect', function(){
+            console.log("Connection has been established using Socket....");
+        })
+    }
+}
--- config/chatEngine_socket.js
+++ config/chatEngine_socket.js
@@ -1,5 +1,15 @@
 // Socket for server
 
 module.exports.chatSocketIO = function(serverSocket){
+    let io = require('socket.io')(serverSocket);
 
+    // request to send on connection request for sockets
+    io.sockets.on('connection', function(socket){
+        console.log("New Connection received from ",socket.id);
+
+        // when this connection is disconnected/lost due to any reason such as page refresh
+        socket.on('disconnect', function(){
+            console.log("Connection Lost with Socket");
+        }) 
+    });
 }
